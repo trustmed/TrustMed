5f1820767cbcb04321583ef316f02c2c583e63db
Haritha Kuruwitaarachchi <harithathushanka@gmail.com>
2026-03-03 20:57:45 +0530

    Revert "chore: configure orval restful client"
    This reverts commit 9b4ed0a8e1a6d64500be6b0ec8d9043feda860ad.

--- apps/core-frontend/config/api-config/AuthTokenProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useSyncAuthToken } from "./useSyncAuthToken";
-
-export function AuthTokenProvider({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  useSyncAuthToken();
-  return <>{children}</>;
-}
--- apps/core-frontend/config/api-config/authTokenStore.ts
+++
@@ -1,7 +0,0 @@
-let authToken: string | null = null;
-
-export const setAuthToken = (token: string | null) => {
-  authToken = token;
-};
-
-export const getAuthToken = () => authToken;
