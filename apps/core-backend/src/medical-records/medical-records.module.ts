608f2980f08f4ab0779d9826dc8de6582818d9df
Sajini Dilshika <sajini2021e@gmail.com>
2026-03-19 00:19:31 +0530

    fix: medical record upload issue

--- apps/core-backend/src/medical-records/medical-records.module.ts
+++ apps/core-backend/src/medical-records/medical-records.module.ts
@@ -5,6 +5,7 @@ import { memoryStorage } from 'multer';
 import { MedicalRecordsController } from './medical-records.controller';
 import { MedicalRecordsService } from './medical-records.service';
 import { MedicalRecord } from '../entities/medical-record.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { MedicalRecord } from '../entities/medical-record.entity';
       storage: memoryStorage(), // keep file in memory → stream to S3
       limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB
     }),
+    AuthModule,
   ],
   controllers: [MedicalRecordsController],
   providers: [MedicalRecordsService],
