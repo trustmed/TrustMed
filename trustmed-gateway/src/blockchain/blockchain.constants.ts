e96ba81f5855ae348efa989afa65a3fd208fbf83
pasinduvinsuka <nimsarapasindu1@gmail.com>
2026-03-20 11:36:07 +0530

    feat(fabric-gateway): create seperate gateway layer

---
+++ trustmed-gateway/src/blockchain/blockchain.constants.ts
@@ -0,0 +1,47 @@
+import path from 'node:path';
+
+const testNetworkPath =
+  '/home/ubuntu/TrustMed/blockchain/fabric-samples/fabric-samples/test-network';
+
+export const FABRIC = {
+  channelName: 'mychannel',
+  chaincodeName: 'trustmed',
+  mspId: 'Org1MSP',
+
+  certPath: path.join(
+    testNetworkPath,
+    'organizations',
+    'peerOrganizations',
+    'org1.example.com',
+    'users',
+    'Admin@org1.example.com',
+    'msp',
+    'signcerts',
+    'cert.pem',
+  ),
+
+  keyDirPath: path.join(
+    testNetworkPath,
+    'organizations',
+    'peerOrganizations',
+    'org1.example.com',
+    'users',
+    'Admin@org1.example.com',
+    'msp',
+    'keystore',
+  ),
+
+  tlsCertPath: path.join(
+    testNetworkPath,
+    'organizations',
+    'peerOrganizations',
+    'org1.example.com',
+    'peers',
+    'peer0.org1.example.com',
+    'tls',
+    'ca.crt',
+  ),
+
+  peerEndpoint: 'localhost:7051',
+  peerHostAlias: 'peer0.org1.example.com',
+};
