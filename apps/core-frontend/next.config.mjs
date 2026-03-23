43aad08be2a9df2671c45e13cb9c58a74b68ac25
Pramod Maneesha <pramod.20223065@iit.ac.lk>
2026-03-23 05:39:15 +0530

    fix: consent request acceptance api related issue

--- apps/core-frontend/next.config.mjs
+++ apps/core-frontend/next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
     unoptimized: true,
   },
   transpilePackages: ["@trustmed/components"],
+    async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
